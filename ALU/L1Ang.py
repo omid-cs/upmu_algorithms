9cf2c48c7aa343feafc4ceceb15cfd95897748ff
Connor Brooks <connoc706@gmail.com>
2014-10-02 15:43:56 -0700

    Corrected input-stream dates again

--- ALU/L1Ang.py
+++ ALU/L1Ang.py
@@ -38,8 +38,8 @@ class ExampleDelta(qdf.QuasarDistillate):
             print "Already done"
             return 
 
-        start_date = self.date("2014-8-16T00:00:00.000000")
-        end_date = self.date("2014-8-17T00:00:00.000000")
+        start_date = self.date("2014-8-17T00:00:00.000000")
+        end_date = self.date("2014-8-18T00:00:00.000000")
 
         hz1_version, hz1_values = yield self.stream_get("1hz", start_date, end_date)
         hz2_version, hz2_values = yield self.stream_get("2hz", start_date, end_date)
