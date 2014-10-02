25ebdd6b2f44898f71c618e97b339d4547744f9d
yijianlu <yijianlu@berkeley.edu>
2014-10-01 20:18:01 -0700

    Update L1_building7-grizzy.py

--- ALU/L1_building7-grizzy.py
+++ ALU/L1_building7-grizzy.py
@@ -24,7 +24,7 @@ class ExampleDelta(qdf.QuasarDistillate):
 
         #If this is incremented, it is assumed that the whole distillate is invalidated, and it
         #will be deleted and discarded. In addition all 'persist' data will be removed
-        self.set_version(7)
+        self.set_version(8)
 
     @defer.inlineCallbacks
     def compute(self):
@@ -38,8 +38,8 @@ class ExampleDelta(qdf.QuasarDistillate):
             print "Already done"
             return
 
-        start_date = self.date("2014-08-24T00:00:00.000000")
-        end_date = self.date("2014-09-24T00:00:00.000000")
+        start_date = self.date("2014-09-01T00:00:00.000000")
+        end_date = self.date("2014-09-02T00:00:00.000000")
 
         hz1_version, hz1_values = yield self.stream_get("1hz", start_date, end_date)
         hz2_version, hz2_values = yield self.stream_get("2hz", start_date, end_date)
