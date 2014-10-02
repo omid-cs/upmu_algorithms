1431a31ee9efea92813e684d78ac0450226b3938
Connor Brooks <cbrooks@berkeley.edu>
2014-10-02 16:09:40 -0700

    Reduced Time Range again

--- ALU/L1Ang.py
+++ ALU/L1Ang.py
@@ -39,7 +39,7 @@ class ExampleDelta(qdf.QuasarDistillate):
             return 
 
         start_date = self.date("2014-08-17T00:00:00.000000")
-        end_date = self.date("2014-08-17T06:00:00.000000")
+        end_date = self.date("2014-08-17T00:30:00.000000")
 
         hz1_version, hz1_values = yield self.stream_get("1hz", start_date, end_date)
         hz2_version, hz2_values = yield self.stream_get("2hz", start_date, end_date)
