09c621d5acb5326f6795b13330819960baad25ab
yijianlu <yijianlu@berkeley.edu>
2014-10-01 20:00:11 -0700

    Update L1_building7-grizzy.py

--- ALU/L1_building7-grizzy.py
+++ ALU/L1_building7-grizzy.py
@@ -24,7 +24,7 @@ class ExampleDelta(qdf.QuasarDistillate):
 
         #If this is incremented, it is assumed that the whole distillate is invalidated, and it
         #will be deleted and discarded. In addition all 'persist' data will be removed
-        self.set_version(2)
+        self.set_version(6)
 
     @defer.inlineCallbacks
     def compute(self):
