cf02068ce8eb75ac047bcf638338cc12854152f7
yijianlu <yijianlu@berkeley.edu>
2014-10-03 15:10:05 -0700

    Update L1_difference.py

--- ALU/L1_difference.py
+++ ALU/L1_difference.py
@@ -17,14 +17,17 @@ class ExampleDelta(qdf.QuasarDistillate):
         self.set_name("L1difference")
 
         #This is the final level. You can have multiple of these
-        self.add_stream("L1ang", unit="Degrees")
-
+        self.add_stream("L1ang_GB", unit="Degrees")
+        self.add_stream("L1ang_GS", unit="Degrees")
+        self.add_stream("L1ang_BS", unit="Degrees")
         self.use_stream("1hz", "b4776088-2f85-4c75-90cd-7472a949a8fa")
         self.use_stream("2hz", "66fcb659-c69a-41b5-b874-80ac7d7f669d")
+        self.use_stream("3hz", "adf13e17-44b7-4ef6-ae3f-fde8a9152ab7")
+        
 
         #If this is incremented, it is assumed that the whole distillate is invalidated, and it
         #will be deleted and discarded. In addition all 'persist' data will be removed
-        self.set_version(2)
+        self.set_version(3)
 
     @defer.inlineCallbacks
     def compute(self):
@@ -43,6 +46,7 @@ class ExampleDelta(qdf.QuasarDistillate):
 
         hz1_version, hz1_values = yield self.stream_get("1hz", start_date, end_date)
         hz2_version, hz2_values = yield self.stream_get("2hz", start_date, end_date)
+        hz3_version, hz3_values = yield self.stream_get("3hz", start_date, end_date)
         delta_values = []
 
         idx1 = 0
@@ -61,14 +65,62 @@ class ExampleDelta(qdf.QuasarDistillate):
                 delta=delta+360
             delta_values.append((hz1_values[idx1].time, delta))
             if len(delta_values) >= qdf.OPTIMAL_BATCH_SIZE:
-                yield self.stream_insert_multiple("L1ang", delta_values)
+                yield self.stream_insert_multiple("L1ang_GB", delta_values)
                 delta_values = []
             idx1 += 1
             idx2 += 1
 
-        yield self.stream_insert_multiple("L1ang", delta_values)
+        yield self.stream_insert_multiple("L1ang_GB", delta_values)
+        
+        delta_values = []
 
+        idx1 = 0
+        idx2 = 0
+        while idx1 < len(hz1_values) and idx2 < len(hz3_values):
+            if hz1_values[idx1].time < hz3_values[idx2].time:
+                idx1 += 1
+                continue
+            if hz1_values[idx1].time > hz3_values[idx2].time:
+                idx2 += 1
+                continue
+            delta = hz1_values[idx1].value - hz3_values[idx2].value
+            if delta > 180:
+                delta =delta-360
+            if delta <-180:
+                delta=delta+360
+            delta_values.append((hz1_values[idx1].time, delta))
+            if len(delta_values) >= qdf.OPTIMAL_BATCH_SIZE:
+                yield self.stream_insert_multiple("L1ang_GS", delta_values)
+                delta_values = []
+            idx1 += 1
+            idx2 += 1
+
+        yield self.stream_insert_multiple("L1ang_GS", delta_values)
         #Now that we are done, save the time we finished at
+        delta_values = []
+
+        idx1 = 0
+        idx2 = 0
+        while idx1 < len(hz2_values) and idx2 < len(hz3_values):
+            if hz2_values[idx1].time < hz3_values[idx2].time:
+                idx1 += 1
+                continue
+            if hz2_values[idx1].time > hz3_values[idx2].time:
+                idx2 += 1
+                continue
+            delta = hz2_values[idx1].value - hz3_values[idx2].value
+            if delta > 180:
+                delta =delta-360
+            if delta <-180:
+                delta=delta+360
+            delta_values.append((hz2_values[idx1].time, delta))
+            if len(delta_values) >= qdf.OPTIMAL_BATCH_SIZE:
+                yield self.stream_insert_multiple("L1ang_BS", delta_values)
+                delta_values = []
+            idx1 += 1
+            idx2 += 1
+
+        yield self.stream_insert_multiple("L1ang_BS", delta_values)
         self.persist("done", True)
     
 
