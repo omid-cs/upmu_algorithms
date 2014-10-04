89df3b5681f91d75ba135f4cc44a46d3c43a6a9d
Connor Brooks <cbrooks@berkeley.edu>
2014-10-03 21:33:25 -0700

    Update clean_distillate_streams_magnitude.py

--- CAB/clean_distillate_streams_magnitude.py
+++ CAB/clean_distillate_streams_magnitude.py
@@ -15,27 +15,26 @@ def clean(input_streams):
   while i < len(input_points):
     if input_points[i].value >= lower_bound and input_points[i].value <= upper_bound:
       clean_points.append((input_points[i].time, input_points[i].value))
-      error_points.append((input_points[i].time, 0))
     else:
       error_points.append((input_points[i].time, 1))
 
-    if len(error_points) >= qdf.OPTIMAL_BATCH_SIZE:
+    if len(error_points) + len(clean_points) >= qdf.OPTIMAL_BATCH_SIZE:
       yield [clean_points, error_points] # keep order as specified in opts
       clean_points = []
       error_points = []
     i += 1
-
+  yield [clean_points, error_points]
   yield None
 
 opts = { 'input_streams'  : ['upmu/grizzly_new/L1MAG'], \
          'input_uids'     : ['a64c386e-2dd4-4f17-96cb-1655358cb12c'], \
-         'start_date'     : '2014-08-28T00:00:00.000000', \
-         'end_date'       : '2014-08-29T00:00:00.000000', \
+         'start_date'     : '2014-09-07T00:00:00.000000', \
+         'end_date'       : '2014-09-08T00:00:00.000000', \
          'output_streams' : ['grizzly_new_L1Mag_clean', 'grizzly_new_L1Mag_errors'], \
          'output_units'   : ['V', 'error'], \
          'author'         : 'CAB', \
          'name'           : 'Clean', \
-         'version'        : 3, \
+         'version'        : 2, \
          'algorithm'      : clean }
 qdf.register(Distillate(), opts)
 qdf.begin()
