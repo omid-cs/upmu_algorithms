e1902cba38ac59f627d02004625a96867c3ec97f
Connor Brooks <cbrooks@berkeley.edu>
2014-10-03 17:43:13 -0700

    Adjusted opts dictionary to included new keys

--- CAB/scale.py
+++ CAB/scale.py
@@ -7,11 +7,13 @@ def scale(input_streams, output_streams):
   
   # only one output stream
   output_stream = output_streams[0]
+  
+  scale_factor = 9
 
   scaled_values = []
   i = 0
   while i < len(input_values):
-    scaled_value = input_values[i].value * self.scale_factor
+    scaled_value = input_values[i].value * scale_factor
     scaled_values.append((input_values[i].time, scaled_value))
     if len(scaled_values) >= qdf.OPTIMAL_BATCH_SIZE:
       yield self.stream_insert_multiple(self.output_stream, scaled_values)
@@ -20,14 +22,15 @@ def scale(input_streams, output_streams):
 
   yield self.stream_insert_multiple(self.output_stream, scaled_values)  
 
-opts = { 'input_streams'  : ['L1MAG'], \
-         'input_uids'     : ['abffcf07-9e17-404a-98c3-ea4d60042ff3'], \
+opts = { 'input_streams'  : ['upmu/grizzly_new/L1MAG'], \
+         'input_uids'     : ['a64c386e-2dd4-4f17-96cb-1655358cb12c'], \
          'start_dates'    : ['2014-09-07T00:00:00.000000'], \
          'end_dates'      : ['2014-09-08T00:00:00.000000'], \
-         'output_streams' : ['soda_a_L1MAG_6.0'], \
+         'output_streams' : ['grizzly_new_L1Mag_scale_9.0'], \
          'output_units'   : ['V'], \
-         'scale_factor'   : 6.0, \
-         'version'        : 5, \
+         'author'         : 'CAB', \
+         'name'           : 'Scale', \
+         'version'        : 9.0, \
          'algorithm'      : scale }
 qdf.register(Distillate(), opts)
 qdf.begin()
