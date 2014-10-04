ab0058832d593c52f8360a79ce5af361c04055bf
Connor Brooks <cbrooks@berkeley.edu>
2014-10-03 21:50:26 -0700

    Update scale.py

--- CAB/scale.py
+++ CAB/scale.py
@@ -27,7 +27,7 @@ opts = { 'input_streams'  : ['upmu/grizzly_new/L1MAG'], \
          'output_units'   : ['V'], \
          'author'         : 'CAB', \
          'name'           : 'Scale', \
-         'version'        : 13, \
+         'version'        : 14, \
          'algorithm'      : scale }
 qdf.register(Distillate(), opts)
 qdf.begin()
