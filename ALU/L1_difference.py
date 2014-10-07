dd973c4d863577a625349439b4c327ec420d7276
yijianlu <yijianlu@berkeley.edu>
2014-10-07 14:39:05 -0700

    Update L1_difference.py

--- ALU/L1_difference.py
+++ ALU/L1_difference.py
@@ -27,7 +27,7 @@ class ExampleDelta(qdf.QuasarDistillate):
 
         #If this is incremented, it is assumed that the whole distillate is invalidated, and it
         #will be deleted and discarded. In addition all 'persist' data will be removed
-        self.set_version(5)
+        self.set_version(6)
 
     @defer.inlineCallbacks
     def compute(self):
