90970a6601a94cf1f77b361b97f63c1f536312f0
Connor Brooks <cbrooks@berkeley.edu>
2014-10-08 14:41:48 -0700

    Update date and version

--- CAB/frequency.py
+++ CAB/frequency.py
@@ -54,13 +54,13 @@ def frequency(input_streams):
 
 opts = { 'input_streams'  : ['upmu/grizzly_new/L2ANG'], \
          'input_uids'     : ['8b80c070-7bb1-44d3-b3a8-301558d573ea'], \
-         'start_date'     : '2014-09-07T00:00:00.000000', \
-         'end_date'       : '2014-09-07T08:00:00.000000', \
+         'start_date'     : '2014-10-06T01:30:00.000000', \
+         'end_date'       : '2014-10-06T02:30:00.000000', \
          'output_streams' : ['grizzly_new_L2Ang_frequency'], \
          'output_units'   : ['Hz'], \
          'author'         : 'CAB', \
          'name'           : 'New Output Frequency', \
-         'version'        : 5, \
+         'version'        : 6, \
          'algorithm'      : frequency }
 qdf.register(Distillate(), opts)
 qdf.begin()
