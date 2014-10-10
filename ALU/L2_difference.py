4c2d5b1be405d02ebe0b32b909fa0b1e385e133a
yijianlu <yijianlu@berkeley.edu>
2014-10-10 16:11:08 -0700

    Update L2_difference.py

--- ALU/L2_difference.py
+++ ALU/L2_difference.py
@@ -28,7 +28,7 @@ class ExampleDelta(qdf.QuasarDistillate):
         self.use_stream("5hz", "321db464-b05b-4a97-988c-1a9cc5593143")
         #If this is incremented, it is assumed that the whole distillate is invalidated, and it
         #will be deleted and discarded. In addition all 'persist' data will be removed
-        self.set_version(5)
+        self.set_version(6)
 
     @defer.inlineCallbacks
     def compute(self):
@@ -127,7 +127,7 @@ class ExampleDelta(qdf.QuasarDistillate):
 
         idx1 = 0
         idx2 = 0
-        while idx1 < len(hz5_values) and idx2 < len(hz4_values):
+        while idx1 < len(hz4_values) and idx2 < len(hz5_values):
             if hz4_values[idx1].time < hz5_values[idx2].time:
                 idx1 += 1
                 continue
