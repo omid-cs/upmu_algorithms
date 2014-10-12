823eca68db6da0fa0295966235abb8e0b430f4e6
Connor Brooks <cbrooks@berkeley.edu>
2014-10-11 21:48:04 -0700

    Update scale.py

--- CAB/scale.py
+++ CAB/scale.py
@@ -18,13 +18,13 @@ def scale(input_streams):
 
 opts = { 'input_streams'  : ['upmu/grizzly_new/L1MAG'], \
          'input_uids'     : ['a64c386e-2dd4-4f17-96cb-1655358cb12c'], \
-         'start_date'     : '2014-09-07T00:00:00.000000', \
-         'end_date'       : '2014-09-08T00:00:00.000000', \
+         'start_date'     : '2014-08-28T00:00:00.000000', \
+         'end_date'       : '2014-08-28T02:00:00.000000', \
          'output_streams' : ['grizzly_new_L1Mag_scale_1.2'], \
          'output_units'   : ['V'], \
          'author'         : 'CAB', \
-         'name'           : 'Scale', \
-         'version'        : 15, \
+         'name'           : 'Test1', \
+         'version'        : 5, \
          'algorithm'      : scale }
 qdf.register(Distillate(), opts)
 qdf.begin()
