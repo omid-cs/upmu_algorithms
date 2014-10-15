aaf27e9308b02bc63d6fa195e99d18ea586c8b68
yijianlu <yijianlu@berkeley.edu>
2014-10-15 14:34:17 -0700

    Update L1_difference.py

--- ALU/L1_difference.py
+++ ALU/L1_difference.py
@@ -29,7 +29,7 @@ class ExampleDelta(qdf.QuasarDistillate):
         
         #If this is incremented, it is assumed that the whole distillate is invalidated, and it
         #will be deleted and discarded. In addition all 'persist' data will be removed
-        self.set_version(12)
+        self.set_version(13)
 
     @defer.inlineCallbacks
     def compute(self):
@@ -44,7 +44,7 @@ class ExampleDelta(qdf.QuasarDistillate):
             return 
 
         start_date = self.date("2014-10-01T00:00:00.000000")
-        end_date = self.date("2014-10-02T00:00:00.000000")
+        end_date = self.date("2014-10-01T00:30:00.000000")
 
         hz1_version, hz1_values = yield self.stream_get("1hz", start_date, end_date)
         hz2_version, hz2_values = yield self.stream_get("2hz", start_date, end_date)
