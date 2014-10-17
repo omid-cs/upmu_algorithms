c48b5227707ed1060c043b5c1dd30ab66e02142a
yijianlu <yijianlu@berkeley.edu>
2014-10-17 13:58:45 -0700

    Update L1_difference.py

--- ALU/L1_difference.py
+++ ALU/L1_difference.py
@@ -2,8 +2,7 @@ from distillate import Distillate
 import numpy as np
 import qdf
 
-
-    def compute(self):
+def compute(self):
         
         grizzly = input_streams[0]
         building71=input_streams[1]
