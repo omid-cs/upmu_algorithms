d79d0c21e106ecc77864cbd8036c4ded635f3ce7
yijianlu <yijianlu@berkeley.edu>
2014-10-21 22:33:00 -0700

    Update seq_grizzly.py

--- ALU/seq_grizzly.py
+++ ALU/seq_grizzly.py
@@ -1,5 +1,6 @@
 from distillate import Distillate
 import numpy as np
+import math
 import qdf
 
 def compute(input_streams):
@@ -27,38 +28,96 @@ def compute(input_streams):
         and idxL3A < len(L3Ang) and idxL3M < len(L3Mag): 
             if L1Ang[idxL1A].time < L2Ang[idxL2A].time:
                 idxL1A += 1
-                idxL1M += 1
                 continue
             if L1Ang[idxL1A].time > L2Ang[idxL2A].time:
                 idxL2A += 1
-                idxL2M += 1
                 continue
             if L1Ang[idxL1A].time < L3Ang[idxL3A].time:
                 idxL1A += 1
-                idxL1M += 1
                 continue
             if L1Ang[idxL1A].time > L3Ang[idxL3A].time:
                 idxL3A += 1
-                idxL3M += 1
                 continue
             if L2Ang[idxL2A].time < L3Ang[idxL3A].time:
                 idxL2A += 1
-                idxL2M += 1
                 continue
             if L2Ang[idxL2A].time > L3Ang[idxL3A].time:
                 idxL3A += 1
+                continue
+            if L1Mag[idxL1M].time < L2Mag[idxL2M].time:
+                idxL1M += 1
+                continue
+            if L1Mag[idxL1M].time > L2Mag[idxL2M].time:
+                idxL2M += 1
+                continue
+            if L1Mag[idxL1M].time < L3Mag[idxL3M].time:
+                idxL1M += 1
+                continue
+            if L1Mag[idxL1M].time > L3Mag[idxL3M].time:
                 idxL3M += 1
                 continue
-            v0m=(L1Mag[idxL1M].value+L2Mag[idxL2M].value+L3Mag[idxL3M].value)/3.0
-            V0Mag.append((L1Mag[idxL1M].time, v0m))
-            VpMag.append((L1Mag[idxL1M].time, v0m))
-            VnMag.append((L1Mag[idxL1M].time, v0m))
-            v0a=(L1Ang[idxL1A].value+L2Ang[idxL2A].value+L3Ang[idxL3A].value)/3.0
-            V0Ang.append((L1Ang[idxL1A].time,v0a))
-            vna=(L1Ang[idxL1A].value+L2Ang[idxL2A].value+120+L3Ang[idxL3A].value+240)/3.0
-            VnAng.append((L1Ang[idxL1A].time,vna))
-            vpa=(L1Ang[idxL1A].value+L2Ang[idxL2A].value+240+L3Ang[idxL3A].value+120)/3.0
-            VpAng.append((L1Ang[idxL1A].time,vpa))
+            if L2Mag[idxL2M].time < L3Mag[idxL3M].time:
+                idxL2M += 1
+                continue
+            if L2Mag[idxL2M].time > L3Mag[idxL3M].time:
+                idxL3M += 1
+                continue
+            if L1Mag[idxL1M].time < L1Ang[idxL1A].time:
+                idxL1M += 1
+                continue
+            if L1Mag[idxL1M].time > L1Ang[idxL1A].time:
+                idxL1A += 1
+                continue
+            if L2Mag[idxL2M].time < L2Ang[idxL2A].time:
+                idxL2M += 1
+                continue
+            if L2Mag[idxL2M].time > L2Ang[idxL2A].time:
+                idxL2A += 1
+                continue
+            if L3Mag[idxL3M].time < L3Ang[idxL3A].time:
+                idxL2M += 1
+                continue
+            if L3Mag[idxL3M].time > L3Ang[idxL3A].time:
+                idxL3A += 1
+                continue
+        
+            sinL1Ang=np.sin(np.radians(L1Ang[idxL1A].value))
+            sinL2Ang=np.sin(np.radians(L2Ang[idxL2A].value))
+            sinL3Ang=np.sin(np.radians(L3Ang[idxL3A].value))
+            sinL2Ang_add120=np.sin(np.radians(L2Ang[idxL2A].value+120))
+            sinL2Ang_add240=np.sin(np.radians(L2Ang[idxL2A].value+240))
+            sinL3Ang_add120=np.sin(np.radians(L3Ang[idxL3A].value+120))
+            sinL3Ang_add240=np.sin(np.radians(L3Ang[idxL3A].value+240))
+            
+            cosL1Ang=np.cos(np.radians(L1Ang[idxL1A].value))
+            cosL2Ang=np.cos(np.radians(L2Ang[idxL2A].value))
+            cosL3Ang=np.cos(np.radians(L3Ang[idxL3A].value))
+            cosL2Ang_add120=np.cos(np.radians(L2Ang[idxL2A].value+120))
+            cosL2Ang_add240=np.cos(np.radians(L2Ang[idxL2A].value+240))
+            cosL3Ang_add120=np.cos(np.radians(L3Ang[idxL3A].value+120))
+            cosL3Ang_add240=np.cos(np.radians(L3Ang[idxL3A].value+240))
+            
+            v0imagine=(L1Mag[idxL1M].value*sinL1Ang+L2Mag[idxL2M].value*sinL2Ang+L3Mag[idxL3M].value*sinL3Ang)/3.0
+            v0real=(L1Mag[idxL1M].value*cosL1Ang+L2Mag[idxL2M].value*cosL2Ang+L3Mag[idxL3M].value*cosL3Ang)/3.0
+            v0mag=np.sqrt(v0imagine**2+v0real**2)
+            v0ang=np.degrees(math.atan2(v0imagine,v0real))
+            V0Mag.append((L1Mag[idxL1M].time, v0mag))
+            V0Ang.append((L1Ang[idxL1A].time,v0ang))
+            
+            vnimagine=(L1Mag[idxL1M].value*sinL1Ang+L2Mag[idxL2M].value*sinL2Ang_add120+L3Mag[idxL3M].value*sinL3Ang_add240)/3.0
+            vnreal=(L1Mag[idxL1M].value*cosL1Ang+L2Mag[idxL2M].value*cosL2Ang_add120+L3Mag[idxL3M].value*cosL3Ang_add240)/3.0
+            vnmag=np.sqrt(vnimagine**2+vnreal**2)
+            vnang=np.degrees(math.atan2(vnimagine,vnreal))
+            VnMag.append((L1Mag[idxL1M].time, vnmag))
+            VnAng.append((L1Ang[idxL1A].time,vnang))
+            
+            vpimagine=(L1Mag[idxL1M].value*sinL1Ang+L2Mag[idxL2M].value*sinL2Ang_add240+L3Mag[idxL3M].value*sinL3Ang_add120)/3.0
+            vpreal=(L1Mag[idxL1M].value*cosL1Ang+L2Mag[idxL2M].value*cosL2Ang_add240+L3Mag[idxL3M].value*cosL3Ang_add120)/3.0
+            vpmag=np.sqrt(vpimagine**2+vpreal**2)
+            vpang=np.degrees(math.atan2(vpimagine,vpreal))
+            VpMag.append((L1Mag[idxL1M].time, vpmag))
+            VpAng.append((L1Ang[idxL1A].time,vpang))
+            
             idxL1A+= 1
             idxL1M+= 1
             idxL2A+= 1
@@ -81,7 +140,7 @@ opts = { 'input_streams'  : ['upmu/grizzly_new/L1ANG','upmu/grizzly_new/L1MAG','
          'output_units'   : ['Degree','V','Degree','V','Degree','V'], \
          'author'         : 'Andrew', \
          'name'           : 'Sequense', \
-         'version'        : 2, \
+         'version'        : 3, \
          'algorithm'      : compute }        
 qdf.register(Distillate(), opts)
 qdf.begin()
