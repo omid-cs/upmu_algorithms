a0c892d2d931cd1ad2344d0b33c01444cd9a5f1d
yijianlu <yijianlu@berkeley.edu>
2014-10-31 11:52:24 -0700

    Update seq_grizzly.py

--- ALU/seq_grizzly.py
+++ ALU/seq_grizzly.py
@@ -4,7 +4,7 @@ import math
 import qdf
 
 def compute(input_streams):
-        
+        # data input
         L1Ang = input_streams[0]
         L1Mag=input_streams[1]
         L2Ang=input_streams[2]
@@ -24,6 +24,7 @@ def compute(input_streams):
         idxL3A=0
         idxL3M=0
         
+        # matching time among all 6 data strams to make sure they are at same time before compute sequence
         while idxL1A < len(L1Ang) and idxL1M < len(L1Mag) and idxL2A < len(L2Ang) and idxL2M < len(L2Mag) \
         and idxL3A < len(L3Ang) and idxL3M < len(L3Mag): 
             if L1Ang[idxL1A].time < L2Ang[idxL2A].time:
@@ -80,7 +81,7 @@ def compute(input_streams):
             if L3Mag[idxL3M].time > L3Ang[idxL3A].time:
                 idxL3A += 1
                 continue
-        
+            # compute sin value for three phase and sin value for l2 and l3 anfter add 120 degree and 240 degree
             sinL1Ang=np.sin(np.radians(L1Ang[idxL1A].value))
             sinL2Ang=np.sin(np.radians(L2Ang[idxL2A].value))
             sinL3Ang=np.sin(np.radians(L3Ang[idxL3A].value))
@@ -89,6 +90,7 @@ def compute(input_streams):
             sinL3Ang_add120=np.sin(np.radians(L3Ang[idxL3A].value+120))
             sinL3Ang_add240=np.sin(np.radians(L3Ang[idxL3A].value+240))
             
+            # compute cosin value for three phase and cosin value for l2 and l3 anfter add 120 degree and 240 degree
             cosL1Ang=np.cos(np.radians(L1Ang[idxL1A].value))
             cosL2Ang=np.cos(np.radians(L2Ang[idxL2A].value))
             cosL3Ang=np.cos(np.radians(L3Ang[idxL3A].value))
@@ -97,6 +99,7 @@ def compute(input_streams):
             cosL3Ang_add120=np.cos(np.radians(L3Ang[idxL3A].value+120))
             cosL3Ang_add240=np.cos(np.radians(L3Ang[idxL3A].value+240))
             
+            # compute V0
             v0imagine=(L1Mag[idxL1M].value*sinL1Ang+L2Mag[idxL2M].value*sinL2Ang+L3Mag[idxL3M].value*sinL3Ang)/3.0
             v0real=(L1Mag[idxL1M].value*cosL1Ang+L2Mag[idxL2M].value*cosL2Ang+L3Mag[idxL3M].value*cosL3Ang)/3.0
             v0mag=np.sqrt(v0imagine**2+v0real**2)
@@ -104,6 +107,7 @@ def compute(input_streams):
             V0Mag.append((L1Mag[idxL1M].time, v0mag))
             V0Ang.append((L1Ang[idxL1A].time,v0ang))
             
+            #compute v-
             vnimagine=(L1Mag[idxL1M].value*sinL1Ang+L2Mag[idxL2M].value*sinL2Ang_add120+L3Mag[idxL3M].value*sinL3Ang_add240)/3.0
             vnreal=(L1Mag[idxL1M].value*cosL1Ang+L2Mag[idxL2M].value*cosL2Ang_add120+L3Mag[idxL3M].value*cosL3Ang_add240)/3.0
             vnmag=np.sqrt(vnimagine**2+vnreal**2)
@@ -111,6 +115,7 @@ def compute(input_streams):
             VnMag.append((L1Mag[idxL1M].time, vnmag))
             VnAng.append((L1Ang[idxL1A].time,vnang))
             
+            # compute v+
             vpimagine=(L1Mag[idxL1M].value*sinL1Ang+L2Mag[idxL2M].value*sinL2Ang_add240+L3Mag[idxL3M].value*sinL3Ang_add120)/3.0
             vpreal=(L1Mag[idxL1M].value*cosL1Ang+L2Mag[idxL2M].value*cosL2Ang_add240+L3Mag[idxL3M].value*cosL3Ang_add120)/3.0
             vpmag=np.sqrt(vpimagine**2+vpreal**2)
@@ -124,6 +129,7 @@ def compute(input_streams):
             idxL2M+= 1
             idxL3A+= 1
             idxL3M+= 1
+        # return V0 V+ V- 
         return[V0Ang,V0Mag,VpAng,VpMag,VnAng,VnMag]
         
         
