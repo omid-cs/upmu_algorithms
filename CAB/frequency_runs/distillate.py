7e454463f92f75be0a2d993e055620878aa489e7
Connor Brooks <connoc706@gmail.com>
2014-11-07 14:58:20 -0800

    copied distillate.py into directory

---
+++ CAB/frequency_runs/distillate.py
@@ -0,0 +1,86 @@
+__author__ = 'immesys'
+
+import numpy as np
+import qdf
+from twisted.internet import defer
+
+class Distillate(qdf.QuasarDistillate):
+
+
+    def setup(self, opts):
+        """
+        Initializes distillate parameters options from dictionary 'opts'
+        Creates dataset in appropraite path
+        """
+        #save opts as class attribute
+        self.opts = opts
+
+        #This is the first level in the distillate tree
+        self.set_author(opts['author'])
+
+        #This is the second level. This name should be unique for every algorithm you write
+        self.set_name(opts['name'])
+
+        #This is the final level. You can have multiple of these
+        for i in range(len(opts['output_streams'])):
+          self.add_stream(opts['output_streams'][i], opts['output_units'][i])
+
+        for i in range(len(opts['input_streams'])):
+          self.use_stream(opts['input_streams'][i], opts['input_uids'][i])
+
+        #If this is incremented, it is assumed that the whole distillate is invalidated, and it
+        #will be deleted and discarded. In addition all 'persist' data will be removed
+        self.set_version(opts['version'])
+
+    @defer.inlineCallbacks
+    def compute(self):
+        """
+        This is called to compute your algorithm.
+
+        This distillate scales an input stream by a constant factor
+        """
+
+        if self.unpersist("done",False):
+            print "Already done"
+            return
+
+        input_versions = []
+        input_streams = []
+        
+        current_date = self.date(self.opts['start_date'])
+        end_date = self.date(self.opts['end_date'])
+
+        # take 15 minute windows
+        while current_date < end_date:
+          window_end = current_date + 15 * qdf.MINUTE
+
+          # final window edge case
+          if window_end > end_date:
+            window_end = end_date
+          
+          # fill input_streams array with all streams for a 15 minute time window
+          for i in range(len(self.opts['input_streams'])):
+            input_name = self.opts['input_streams'][i]
+            input_version, input_values = yield self.stream_get(input_name, current_date, window_end)
+            input_versions.append(input_version)
+            input_streams.append(input_values)
+
+          #opts['algorithm']() returns the output data for the 15 minute window
+          #list of output streams is in the same order as was given in opts['output_streams']
+          output_data_list = self.opts['algorithm'](input_streams)
+          for i in range(len(output_data_list)):
+            output_data = output_data_list[i]
+            start = 0
+            while start < len(output_data):
+              self.stream_insert_multiple(self.opts['output_streams'][i],output_data[start:start+qdf.OPTIMAL_BATCH_SIZE])
+              start += qdf.OPTIMAL_BATCH_SIZE
+
+          
+          #reset input versions and streams
+          input_versions = []
+          input_streams = []
+
+          current_date += 15 * qdf.MINUTE
+
+        #Now that we are done, save the time we finished at
+        self.persist("done", True)
