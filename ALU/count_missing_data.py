f9dac957fb72652bca22ed2ec8cef39693c3831f
yijianlu <yijianlu@berkeley.edu>
2014-11-12 23:08:25 -0800

    Update count_missing_data.py

--- ALU/count_missing_data.py
+++ ALU/count_missing_data.py
@@ -45,12 +45,12 @@ opts = { 'input_streams'  : ['upmu/grizzly_new/L1ANG','upmu/grizzly_new/L2ANG','
          'input_uids'     : ['b4776088-2f85-4c75-90cd-7472a949a8fa','8b80c070-7bb1-44d3-b3a8-301558d573ea',
                              'b653c63b-4acc-45ee-ae3d-1602e6116bc1'], \
          'start_date'     : '2014-10-01T00:00:00.000000', \
-         'end_date'       : '2014-10-01T00:01:00.000000', \
+         'end_date'       : '2014-10-01T00:00:10.000000', \
          'output_streams' : ['grizzly_LA','grizzly_LB','grizzly_LC'], \
          'output_units'   : ['Degree','Degree','Degree'], \
          'author'         : 'Andrew', \
          'name'           : 'count missing data', \
-         'version'        : 8, \
+         'version'        : 9, \
          'algorithm'      : compute }        
 qdf.register(Distillate(), opts)
 qdf.begin()
