68a0916f49f041ce342d4d791781abf27f9fee37
yijianlu <yijianlu@berkeley.edu>
2014-11-12 19:03:44 -0800

    Update count_missing_data.py

--- ALU/count_missing_data.py
+++ ALU/count_missing_data.py
@@ -7,70 +7,13 @@ def compute(input_streams):
         LB = input_streams[0]
         LC=input_streams[1]
         LA=input_streams[2]
-        raw_LB=[]
-        raw_LC=[]
-        raw_LA=[]
-        good_LB=[]
-        good_LC=[]
-        good_LA=[]
-        bad_LB=[]
-        bad_LC=[]
-        bad_LA=[]
+        
         idx=0
         while idx < len(LB):
-         raw_LB.append(LB[idx].value)
-         idx+=1
-        raw_LB=np.asarray(raw_LB)
-        r = boxplot(raw_LB)
-        top_points_LB = r["fliers"][0].get_data()[1]
-        bottom_pionts_LB = r["fliers"][2].get_data()[1]
-        bottom_pionts_LB = list(bottom_pionts_LB)
-        top_points_LB = list(top_points_LB)
-        outliers_LB=bottom_pionts_LB+top_pionts_LB
-        idx=0
-        while idx < len(LA):
-         raw_LA.append(LA[idx].value)
-         idx+=1
-        raw_LA=np.asarray(raw_LA)
-        r = boxplot(raw_LA)
-        top_points_LA = r["fliers"][0].get_data()[1]
-        bottom_pionts_LA = r["fliers"][2].get_data()[1]
-        bottom_pionts_LA = list(bottom_pionts_LA)
-        top_points_LA = list(top_points_LA)
-        outliers_LA=bottom_pionts_LA+top_pionts_LA
-        idx=0
-        while idx < len(LC):
-         raw_LC.append(LC[idx].value)
-         idx+=1
-        raw_LC=np.asarray(raw_LC)
-        r = boxplot(raw_LC)
-        top_points_LC = r["fliers"][0].get_data()[1]
-        bottom_pionts_LC = r["fliers"][2].get_data()[1]
-        bottom_pionts_LC = list(bottom_pionts_LC)
-        top_points_LC = list(top_points_LC)
-        outliers_LC=bottom_pionts_LA+top_pionts_LC
-        idx=0
-        while idx < len(LA):
-         if LA[idx].value in outliers_LA:
-            bad_LA.append((LA[idx].time, LA[idx].value))              
-         else:
-            good_LA.append((LA[idx].time, LA[idx].value))     
-         idx+=1    
-        idx=0
-        while idx < len(LB):
-         if LB[idx].value in outliers_LB:
-            bad_LB.append((LB[idx].time, LB[idx].value))              
-         else:
-            good_LB.append((LB[idx].time, LB[idx].value))     
-         idx+=1  
-        idx=0
-        while idx < len(LC):
-         if LC[idx].value in outliers_LC:
-            bad_LC.append((LC[idx].time, LC[idx].value))              
-         else:
-            good_LC.append((LC[idx].time, LC[idx].value))     
-         idx+=1        
-        return[good_LA,bad_LA,good_LB,bad_LB,good_LC,bad_LC]
+         print LB[idx].time
+         break
+       
+        return[good_LA,bad_LA,good_LB]
         
         
         
@@ -80,10 +23,10 @@ opts = { 'input_streams'  : ['upmu/grizzly_new/L1ANG','upmu/grizzly_new/L2ANG','
                              'b653c63b-4acc-45ee-ae3d-1602e6116bc1'], \
          'start_date'     : '2014-10-01T00:00:00.000000', \
          'end_date'       : '2014-10-01T00:00:10.000000', \
-         'output_streams' : ['good_LA','bad_LA','good_LB','bad_LB','good_LC','bad_LC'], \
-         'output_units'   : ['Degree','Degree','Degree','Degree','Degree','Degree'], \
+         'output_streams' : ['LA','LB','LC'], \
+         'output_units'   : ['Degree','Degree','Degree'], \
          'author'         : 'Andrew', \
-         'name'           : 'Remove Outlier', \
+         'name'           : 'count missing data', \
          'version'        : 2, \
          'algorithm'      : compute }        
 qdf.register(Distillate(), opts)
