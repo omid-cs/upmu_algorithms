3d68abe348b8c5c3546c5f4687d48d0cbb8c529b
yijianlu <yijianlu@berkeley.edu>
2014-11-13 21:20:42 -0800

    Update count_missing_data.py

--- ALU/count_missing_data.py
+++ ALU/count_missing_data.py
@@ -48,7 +48,7 @@ def compute(input_streams):
 opts = { 'input_streams'  : ['upmu/grizzly_new/L1ANG','upmu/grizzly_new/L2ANG','upmu/grizzly_new/L3ANG'],\
          'input_uids'     : ['b4776088-2f85-4c75-90cd-7472a949a8fa','8b80c070-7bb1-44d3-b3a8-301558d573ea',
                              'b653c63b-4acc-45ee-ae3d-1602e6116bc1'], \
-         'start_date'     : '2014-10-01T00:00:00.000000', \
+         'start_date'     : '2014-10-01T00:59:50.000000', \
          'end_date'       : '2014-10-31T00:00:00.000000', \
          'output_streams' : ['grizzly_LA','grizzly_LB','grizzly_LC'], \
          'output_units'   : ['Degree','Degree','Degree'], \
