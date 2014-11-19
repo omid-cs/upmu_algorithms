f319586fd0cb324b4bf18e27bf91c5952a70f997
Connor Brooks <connoc706@gmail.com>
2014-11-19 13:56:04 -0800

    reduced time range

--- CAB/dev/copy_test.py
+++ CAB/dev/copy_test.py
@@ -11,7 +11,7 @@ def copy(input_streams, output_streams):
   copy_stream = output_streams[0]
 
   i = 0
-  for i in range(120*60*60*10): # 10 hours of datapoints
+  for i in range(120*60*60*1): # 1 hour of datapoints
     datapoint = stream[i]
     if datapoint != None:
       yield copy_stream.append(datapoint)
@@ -20,7 +20,7 @@ def copy(input_streams, output_streams):
 opts = { 'input_streams'  : ['B71_L2ANG'],
          'input_uids'     : ['f89e77a8-661e-49d2-a868-2071c1fae238'],
          'start_dates'    : ['2014-10-01T00:00.000000'],
-         'end_dates'      : ['2014-10-01T10:00.000000'],
+         'end_dates'      : ['2014-10-01T01:00.000000'],
          'output_streams' : ['B71_L2ANG_copy'],
          'output_units'   : ['Degrees'],
          'author'         : 'CAB',
