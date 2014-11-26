4779489ca9aee7e1b07ba6ee057a676122d53abf
yijianlu <yijianlu@berkeley.edu>
2014-11-26 14:27:32 -0800

    Update library.py

--- ALU/library.py
+++ ALU/library.py
@@ -14,15 +14,15 @@ def DPF(input_streams):
         idxC=0
         idxL=0
         while idxC < len(Input[stream_number]) and idxL < len(Input[stream_number+1]):
-            if Input[stream_number].time < Input[stream_number+1].time:
+            if Input[stream_number][idxC].time < Input[stream_number+1][idxL].time:
                 idxC += 1
                 continue
-            if Input[stream_number].time > Input[stream_number+1].time:
+            if Input[stream_number][idxC].time > Input[stream_number+1][idxL].time:
                 idxL += 1
                 continue
            # compute cosin value of the differnece between voltage angle and current angle and dpf
-            dpf=(np.cos(np.radians(Input[stream_number+1].value-Input[stream_number].value)))*100
-            out.append((Input[stream_number].time,dpf))
+            dpf=(np.cos(np.radians(Input[stream_number+1][idxL].value-Input[stream_number][idxC].value)))*100
+            out.append((Input[stream_number][idxC].time,dpf))
             idxC+=1
             idxL+=1
         Out.append(out) 
