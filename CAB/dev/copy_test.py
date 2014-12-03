8118f82a0cecd58adbfffe3e1ea2a310ddae984f
Connor Brooks <connoc706@gmail.com>
2014-12-03 14:45:25 -0800

    version update

--- CAB/dev/copy_test.py
+++ CAB/dev/copy_test.py
@@ -27,7 +27,7 @@ opts = { 'input_streams'  : ['B71_L2ANG'],
          'output_units'   : ['Degrees'],
          'author'         : 'CAB',
          'name'           : 'Dev',
-         'version'        : 13,
+         'version'        : 14,
          'algorithm'      : copy }
 qdf.register(Distillate(), opts)
 qdf.begin()
