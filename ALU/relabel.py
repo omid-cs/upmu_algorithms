720a392c756a563623d1bdfc3ec64bd2b6223b9f
yijianlu <yijianlu@berkeley.edu>
2014-12-02 21:24:31 -0800

    Update relabel.py

--- ALU/relabel.py
+++ ALU/relabel.py
@@ -22,48 +22,8 @@ def compute(input_streams):
         idxLB=0
         idxCC=0
         idxLC=0
-        # matching time among all 6 data strams to make sure they are at same time before compute sequence
-        while idxCA < len(CAAng) and idxLA < len(LAAng):
-            if CAAng[idxCA].time < LAAng[idxLA].time:
-                idxCA += 1
-                continue
-            if CAAng[idxCA].time > LAAng[idxLA].time:
-                idxLA += 1
-                continue
-           # compute cosin value of the differnece between voltage angle and current angle and dpf
-            dpfa=(np.cos(np.radians(LAAng[idxLA].value-CAAng[idxCA].value)))*100
-            DPF_A.append((LAAng[idxLA].time,dpfa))
-            idxCA+=1
-            idxLA+=1
-            
-        while idxCB < len(CBAng) and idxLB < len(LBAng):
-            if CBAng[idxCB].time < LBAng[idxLB].time:
-                idxCB += 1
-                continue
-            if CBAng[idxCB].time > LBAng[idxLB].time:
-                idxLB += 1
-                continue
-           # compute cosin value of the differnece between voltage angle and current angle and dpf
-            dpfb=(np.cos(np.radians(LBAng[idxLB].value-CBAng[idxCB].value)))*100
-            DPF_B.append((LBAng[idxLB].time,dpfb))
-            idxCB+=1
-            idxLB+=1
-            
-        while idxCC < len(CCAng) and idxLC < len(LCAng):
-            if CCAng[idxCC].time < LCAng[idxLC].time:
-                idxCC += 1
-                continue
-            if CCAng[idxCC].time > LCAng[idxLC].time:
-                idxLC += 1
-                continue
-           # compute cosin value of the differnece between voltage angle and current angle and dpf
-            dpfc=(np.cos(np.radians(LCAng[idxLC].value-CCAng[idxCC].value)))*100
-            DPF_C.append((LCAng[idxLC].time,dpfc))
-            idxCC+=1
-            idxLC+=1
-            
-        ''' DPF_A,DPF_B,DPF_C'''
-        return[DPF_A,DPF_B,DPF_C]
+        
+        return[CAAng,LAAng,CBAng]
         
         
         
@@ -75,11 +35,11 @@ opts = { 'input_streams'  : ['upmu/grizzly_new/L1ANG','upmu/grizzly_new/C1ANG','
                              'b653c63b-4acc-45ee-ae3d-1602e6116bc1','8b40fe4c-36ee-4b10-8aef-1eef8c471e1d'], \
          'start_date'     : '2014-10-01T00:00:00.000000', \
          'end_date'       : '2014-10-19T00:00:00.000000', \
-         'output_streams' : ['DPF_A_grizzly','DPF_B_grizzly','DPF_C_grizzly'], \
+         'output_streams' : ['CAANG','LAANG','CBANG'], \
          'output_units'   : ['Precent','Precent','Precent'], \
          'author'         : 'Andrew', \
-         'name'           : 'DPF', \
-         'version'        : 4, \
+         'name'           : 'lebel', \
+         'version'        : 1, \
          'algorithm'      : compute }        
 qdf.register(Distillate(), opts)
 qdf.begin()
