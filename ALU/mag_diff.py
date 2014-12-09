e544ff4397e7c12bcb3b3d5d8d1d70ab1a843160
yijianlu <yijianlu@berkeley.edu>
2014-12-08 17:28:43 -0800

    Update mag_diff.py

--- ALU/mag_diff.py
+++ ALU/mag_diff.py
@@ -5,155 +5,46 @@ import qdf
 
 def compute(input_streams):
         # data input
-        grizzly_L1 = input_streams[0]
-        switcha6_L1=input_streams[1]
-        grizzly_L2 = input_streams[2]
-        switcha6_L2=input_streams[3]
-        grizzly_L3 = input_streams[4]
-        switcha6_L3=input_streams[5]
-        grizzly_C1 = input_streams[6]
-        switcha6_C1=input_streams[7]
-        grizzly_C2 = input_streams[8]
-        switcha6_C2=input_streams[9]
-        grizzly_C3 = input_streams[10]
-        switcha6_C3=input_streams[11]
-        L1ang_GS = []
-        L2ang_GS = []
-        L3ang_GS = []
-        C1ang_GS = []
-        C2ang_GS = []
-        C3ang_GS = []
+        grizzly_LM = input_streams[0]
+        switcha6_LM=input_streams[1]
+        grizzly_CM = input_streams[2]
+        switcha6_CM=input_streams[3]
+        LM_GS = []
+        CM_GS = []
+        
         idx1 = 0
         idx2 = 0
         # matching time between data stream and skip the piont when there is no data for that time
-        while idx1 < len(grizzly_L1) and idx2 < len(switcha6_L1):
-            if grizzly_L1[idx1].time < switcha6_L1[idx2].time:
+        while idx1 < len(grizzly_LM) and idx2 < len(switcha6_LM):
+            if grizzly_LM[idx1].time < switcha6_LM[idx2].time:
                 idx1 += 1
                 continue
-            if grizzly_L1[idx1].time > switcha6_L1[idx2].time:
+            if grizzly_LM[idx1].time > switcha6_LM[idx2].time:
                 idx2 += 1
                 continue
-            # compute angle difference 
-            delta = grizzly_L1[idx1].value - switcha6_L1[idx2].value
-            if delta > 180:
-                delta =delta-360
-            if delta <-180:
-                delta=delta+360
-            if delta ==-180:
-                delta=180
-            L2ang_GS.append((grizzly_L1[idx1].time, delta))
+            # compute mag difference 
+            delta = grizzly_LM[idx1].value - switcha6_LM[idx2].value
+            LM_GS.append((grizzly_LM[idx1].time, delta))
             idx1 += 1
             idx2 += 1
             
         idx1 = 0
         idx2 = 0
         # matching time between data stream and skip the piont when there is no data for that time
-        while idx1 < len(grizzly_L2) and idx2 < len(switcha6_L2):
-            if grizzly_L2[idx1].time < switcha6_L2[idx2].time:
+        while idx1 < len(grizzly_CM) and idx2 < len(switcha6_CM):
+            if grizzly_CM[idx1].time < switcha6_CM[idx2].time:
                 idx1 += 1
                 continue
-            if grizzly_L2[idx1].time > switcha6_L2[idx2].time:
+            if grizzly_CM[idx1].time > switcha6_CM[idx2].time:
                 idx2 += 1
                 continue
-            # compute angle difference 
-            delta = grizzly_L2[idx1].value - switcha6_L2[idx2].value
-            if delta > 180:
-                delta =delta-360
-            if delta <-180:
-                delta=delta+360
-            if delta ==-180:
-                delta=180
-            L3ang_GS.append((grizzly_L2[idx1].time, delta))
+            # compute mag difference 
+            delta = grizzly_CM[idx1].value - switcha6_CM[idx2].value
+            CM_GS.append((grizzly_CM[idx1].time, delta))
             idx1 += 1
             idx2 += 1 
         
-        idx1 = 0
-        idx2 = 0
-        # matching time between data stream and skip the piont when there is no data for that time
-        while idx1 < len(grizzly_L3) and idx2 < len(switcha6_L3):
-            if grizzly_L3[idx1].time < switcha6_L3[idx2].time:
-                idx1 += 1
-                continue
-            if grizzly_L3[idx1].time > switcha6_L3[idx2].time:
-                idx2 += 1
-                continue
-            # compute angle difference 
-            delta = grizzly_L3[idx1].value - switcha6_L3[idx2].value
-            if delta > 180:
-                delta =delta-360
-            if delta <-180:
-                delta=delta+360
-            if delta ==-180:
-                delta=180
-            L1ang_GS.append((grizzly_L3[idx1].time, delta))
-            idx1 += 1
-            idx2 += 1 
         
-        idx1 = 0
-        idx2 = 0
-        # matching time between data stream and skip the piont when there is no data for that time
-        while idx1 < len(grizzly_C1) and idx2 < len(switcha6_C1):
-            if grizzly_C1[idx1].time < switcha6_C1[idx2].time:
-                idx1 += 1
-                continue
-            if grizzly_C1[idx1].time > switcha6_C1[idx2].time:
-                idx2 += 1
-                continue
-            # compute angle difference 
-            delta = grizzly_C1[idx1].value - switcha6_C1[idx2].value
-            if delta > 180:
-                delta =delta-360
-            if delta <-180:
-                delta=delta+360
-            if delta ==-180:
-                delta=180
-            C2ang_GS.append((grizzly_C1[idx1].time, delta))
-            idx1 += 1
-            idx2 += 1
-            
-        idx1 = 0
-        idx2 = 0
-        # matching time between data stream and skip the piont when there is no data for that time
-        while idx1 < len(grizzly_C2) and idx2 < len(switcha6_C2):
-            if grizzly_C2[idx1].time < switcha6_C2[idx2].time:
-                idx1 += 1
-                continue
-            if grizzly_C2[idx1].time > switcha6_C2[idx2].time:
-                idx2 += 1
-                continue
-            # compute angle difference 
-            delta = grizzly_C2[idx1].value - switcha6_C2[idx2].value
-            if delta > 180:
-                delta =delta-360
-            if delta <-180:
-                delta=delta+360
-            if delta ==-180:
-                delta=180
-            C3ang_GS.append((grizzly_C2[idx1].time, delta))
-            idx1 += 1
-            idx2 += 1   
-            
-        idx1 = 0
-        idx2 = 0
-        # matching time between data stream and skip the piont when there is no data for that time
-        while idx1 < len(grizzly_C3) and idx2 < len(switcha6_C3):
-            if grizzly_C3[idx1].time < switcha6_C3[idx2].time:
-                idx1 += 1
-                continue
-            if grizzly_C3[idx1].time > switcha6_C3[idx2].time:
-                idx2 += 1
-                continue
-            # compute angle difference 
-            delta = grizzly_C3[idx1].value - switcha6_C3[idx2].value
-            if delta > 180:
-                delta =delta-360
-            if delta <-180:
-                delta=delta+360
-            if delta ==-180:
-                delta=180
-            C1ang_GS.append((grizzly_C3[idx1].time, delta))
-            idx1 += 1
-            idx2 += 1    
         # return angle difference computed   
         return[L1ang_GS,L2ang_GS,L3ang_GS,C1ang_GS,C2ang_GS,C3ang_GS]
         
