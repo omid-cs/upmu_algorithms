913eb013f947e629b8370d71bde2292a2efa599f
yijianlu <yijianlu@berkeley.edu>
2014-12-16 15:38:59 -0800

    Update FUND_POWER.py

--- ALU/FUND_POWER.py
+++ ALU/FUND_POWER.py
@@ -95,22 +95,22 @@ def compute(input_streams):
         
         
     
-opts = { 'input_streams'  : ['upmu/grizzly_new/L1MAG','upmu/grizzly_new/C1MAG','upmu/grizzly_new/L2MAG',
-                             'upmu/grizzly_new/C2MAG','upmu/grizzly_new/L3MAG','upmu/grizzly_new/C3MAG',
-                             'Refined Grizzly/Displacement Power Factor/L1_DPF','Refined Grizzly/Displacement Power Factor/L2_DPF',
-                             'Refined Grizzly/Displacement Power Factor/L3_DPF'], \
-         'input_uids'     : ['a64c386e-2dd4-4f17-96cb-1655358cb12c','425b9c51-9aba-4d1a-a677-85cd7afd6269',
-                             'a002295a-32ee-41a1-8ec4-8657d0d1f943','ca613e9a-1211-4c52-a98f-b8f9f1ce0672',
-                             'db3ea4f7-a337-4874-baeb-17fc2c0cf18b','b1025f33-97fd-45d6-bc0f-80132e1dc756',
-                             '5365a3f8-14fa-4b8a-9a9f-ce49c6cd3106','a060c0ef-bb23-4020-9994-0927462bb42c',
-                             '0426953c-ab67-4637-87e8-8ae3cabbce13'], \
+opts = { 'input_streams'  : ['upmu/switch_a6/L1MAG','upmu/switch_a6/C1MAG','upmu/switch_a6/L2MAG',
+                             'upmu/switch_a6/C2MAG','upmu/switch_a6/L3MAG','upmu/switch_a6/C3MAG',
+                             'Calculated Switch_a6/Displacement Power Factor/L1_DPF','Calculated Switch_a6/Displacement Power Factor/L2_DPF',
+                             'Calculated Switch_a6/Displacement Power Factor/L3_DPF'], \
+         'input_uids'     : ['df64af25-a389-4be9-8061-f87c3616f286','bf8ea2c0-6d04-4cdd-ba4b-0421eac0cabd',
+                             '6e6ad513-ddd2-47fb-98c1-16e6477504fc','51d4801e-0bb6-4040-8e74-e7839be65156',
+                             'bcf38098-0e16-46f2-a9fb-9ce481d7d55b','249b364d-b0a1-4b65-8aca-ffd68565c1de',
+                             '203fff08-38c2-4e4a-a5fb-3a6449734188','25c5f13d-c354-4b6e-8f3f-c347485d4f10',
+                             'a275081e-02d3-4697-8baf-b8abcfd90167'], \
          'start_date'     : '2014-12-03T00:00:00.000000', \
          'end_date'       : '2014-12-03T23:59:59.000000', \
          'output_streams' : ['FUND_POWER_L3','FUND_POWER_L1','FUND_POWER_L2'], \
          'output_units'   : ['Watts','Watts','Watts'], \
-         'author'         : 'Calculated Grizzly', \
+         'author'         : 'Calculated Switch_a6', \
          'name'           : 'Power Flow', \
-         'version'        : 16, \
+         'version'        : 18, \
          'algorithm'      : compute }        
 qdf.register(Distillate(), opts)
 qdf.begin()
