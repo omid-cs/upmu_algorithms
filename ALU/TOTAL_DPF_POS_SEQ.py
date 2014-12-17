f9a26972378ccfd34adb45e0dcbac12837ef2566
yijianlu <yijianlu@berkeley.edu>
2014-12-16 16:21:38 -0800

    Update TOTAL_DPF_POS_SEQ.py

--- ALU/TOTAL_DPF_POS_SEQ.py
+++ ALU/TOTAL_DPF_POS_SEQ.py
@@ -33,16 +33,16 @@ def compute(input_streams):
         
         
     
-opts = { 'input_streams'  : ['Refined Switch_a6/Sequence Components/VOLTAGE_POSITIVE_SEQ_ANG',
-                             'Refined Switch_a6/Sequence Components/CURRENT_POSITIVE_SEQ_ANG'], \
-         'input_uids'     : ['978ffd60-f4ed-4d29-a13d-882f3f0196ff','5cfb3c7c-161e-46d1-b39b-e1a30c3e558e'], \
+opts = { 'input_streams'  : ['Refined Grizzly/Sequence Components/VOLTAGE_POSITIVE_SEQ_ANG',
+                             'Refined Grizzly/Sequence Components/CURRENT_POSITIVE_SEQ_ANG'], \
+         'input_uids'     : ['b70fae9d-8275-4956-8fb0-cf42a2474c7c','f5e8010b-147c-4d9b-940c-57703f4d29e7'], \
          'start_date'     : '2014-12-03T00:00:00.000000', \
          'end_date'       : '2014-12-03T11:59:59.000000', \
          'output_streams' : ['TOTAL_DPF_POS_SEQ'], \
          'output_units'   : ['Precent'], \
-         'author'         : 'Calculated Switch_a6', \
+         'author'         : 'Calculated Grizzly', \
          'name'           : 'Sequence Components', \
-         'version'        : 15, \
+         'version'        : 17, \
          'algorithm'      : compute }        
 qdf.register(Distillate(), opts)
 qdf.begin()
