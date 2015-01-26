dd5ca796ac79bee4ab22d6411e37d5df2b6c6c22
Connor Brooks <connoc706@gmail.com>
2015-01-26 13:59:56 -0800

    added phaseshift algorithm and ini run

---
+++ algs/cab/example.py
@@ -0,0 +1,29 @@
+import qdf
+import numpy as np
+
+class Phaseshift (qdf.QDF2Distillate):
+  def initialize(self, name="phaseshift", shift=0):
+    self.set_section("Development")
+    self.set_name(name)
+    self.set_version(1)
+    self.register_output("out", "arb_units")
+    self.register_input("sinusoid")
+
+    self.shift = shift
+
+  def compute(self, changed_ranges, input_streams, params, report):
+    out = report.output("out")
+
+    print "compute invoked:"
+    print "changed_ranges: ", changed_ranges
+    print "params: ", params
+
+    for point in input_streams["sinusoid"]:
+      time = point[0]
+      val = point[1]
+      val += shift
+      if val > 180:
+        val -= 360
+      out.addreading(time, val)
+
+    out.addbounds(*changed_ranges["sinusoid"])
