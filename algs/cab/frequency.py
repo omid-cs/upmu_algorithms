9639d4a85536455b3fc3b17ec2d4d2ea17875bd9
Connor Brooks <connoc706@gmail.com>
2015-01-26 15:16:22 -0800

    version updates

--- algs/cab/frequency.py
+++ algs/cab/frequency.py
@@ -5,7 +5,7 @@ class Frequency (qdf.QDF2Distillate):
   def initialize(self, name="frequency", output="frequency", dt="1.0"):
     self.set_section("Development")
     self.set_name(name)
-    self.set_version(1)
+    self.set_version(2)
     self.register_output(output, "Hz")
     self.register_input("phase")
 
