de718783a1d1d78b6a1006c4ed537ea759226713
Connor Brooks <connoc706@gmail.com>
2015-02-02 18:16:29 -0800

    removed comments, fixed variable name

--- algs/cab/phase_difference.py
+++ algs/cab/phase_difference.py
@@ -3,31 +3,21 @@ import numpy as np
 
 class Phase_Difference (qdf.QDF2Distillate):
   def initialize(self, section="Phase_Difference", name="default"):
-    print('Begin initializing algorithm')
     self.set_section(section)
     self.set_name(name)
     self.set_version(3)
     self.register_output("phase_difference", "Degrees")
     self.register_input("phase1")
     self.register_input("phase2")
-    print('Finished initializing algorithm')
 
   def compute(self, changed_ranges, input_streams, params, report):
     phase_diff_output = report.output("phase_difference")
 
-    print "[ALG] compute invoked:"
-    print "[ALG] input_streams:"
-    print input_streams
     phase1 = input_streams["phase1"]
     phase2 = input_streams["phase2"]
 
-
-    print "changed_ranges: ", changed_ranges
-    print "params: ", params
-
     i1 = 0
     i2 = 0
-    print "algorithm successfully prepped for calculation"
     while i1 < len(phase1) and i2 < len(phase2):
       if not (phase1[i1][0] == phase2[i2][0]):
         # if times do not align, iteratively increment trailing stream until equal
@@ -47,5 +37,5 @@ class Phase_Difference (qdf.QDF2Distillate):
       i1 += 1
       i2 += 1
 
-    reactive_power_output.addbounds(*changed_ranges["phase1"])
+    phase_diff_output.addbounds(*changed_ranges["phase1"])
     reactive_power_output.addbounds(*changed_ranges["phase2"])
