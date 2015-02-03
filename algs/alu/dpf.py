59a2a0846ab3546e18d890913603a60df08ba187
yijianlu <yijianlu@berkeley.edu>
2015-02-02 20:35:53 -0800

    Update dpf.py

--- algs/alu/dpf.py
+++ algs/alu/dpf.py
@@ -4,7 +4,7 @@ class DPF (qdf.QDF2Distillate):
     def initialize(self, section, name):
         self.set_section(section)
         self.set_name(name)
-        self.set_version(1)
+        self.set_version(2)
         self.register_output("L1_DPF", "Precent")
         self.register_output("L2_DPF", "Precent")
         self.register_output("L3_DPF", "Precent")
@@ -37,44 +37,44 @@ class DPF (qdf.QDF2Distillate):
         C3=input_streams["C3"]
 
         while idxC1 len(C1) and idxL1< len(L1) :
-            if not (C1[idxC1].time == L1[idxL1].time):
-                max_time=max(C1[idxC1].time,L1[idxL1].time)
-                if C1[idxC1].time < max_time:
+            if not (C1[idxC1][0] == L1[idxL1][0]):
+                max_time=max(C1[idxC1][0],L1[idxL1][0])
+                if C1[idxC1][0] < max_time:
                     idxC1 += 1
-                if L1[idxL1].time < max_time:
+                if L1[idxL1][0] < max_time:
                     idxL1 += 1
                 continue
             # compute cosin value of the differnece between voltage angle and current angle and dpf
-            dpf1=(np.cos(np.radians(L1[idxL1].value-C1[idxC1].value)))*100
-            L1_DPF.addreading((L1[idxL1].time,dpf1))
+            dpf1=(np.cos(np.radians(L1[idxL1][1]-C1[idxC1][1])))*100
+            L1_DPF.addreading((L1[idxL1][0],dpf1))
             idxL1+=1
             idxC1+=1
             
         while idxC2 len(C2) and idxL2< len(L2) :
-            if not (C2[idxC2].time == L2[idxL2].time):
-                max_time=max(C2[idxC2].time,L2[idxL2].time)
-                if C2[idxC2].time < max_time:
+            if not (C2[idxC2][0] == L2[idxL2][0]):
+                max_time=max(C2[idxC2][0],L2[idxL2][0])
+                if C2[idxC2][0] < max_time:
                     idxC2 += 1
-                if L2[idxL2].time < max_time:
+                if L2[idxL2][0] < max_time:
                     idxL2 += 1
                 continue
             # compute cosin value of the differnece between voltage angle and current angle and dpf
-            dpf2=(np.cos(np.radians(L2[idxL2].value-C2[idxC2].value)))*100
-            L2_DPF.addreading((L2[idxL2].time,dpf2))
+            dpf2=(np.cos(np.radians(L2[idxL2][1]-C2[idxC2][1])))*100
+            L2_DPF.addreading((L2[idxL2][0],dpf2))
             idxL2+=1
             idxC2+=1
             
         while idxC3 len(C3) and idxL3< len(L3) :
-            if not (C3[idxC3].time == L3[idxL3].time):
-                max_time=max(C3[idxC3].time,L3[idxL3].time)
-                if C3[idxC3].time < max_time:
+            if not (C3[idxC3][0] == L3[idxL3][0]):
+                max_time=max(C3[idxC3][0],L3[idxL3][0])
+                if C3[idxC3][0] < max_time:
                     idxC3 += 1
-                if L3[idxL3].time < max_time:
+                if L3[idxL3][0] < max_time:
                     idxL3 += 1
                 continue
             # compute cosin value of the differnece between voltage angle and current angle and dpf
-            dpf3=(np.cos(np.radians(L3[idxL3].value-C3[idxC3].value)))*100
-            L3_DPF.addreading((L3[idxL3].time,dpf3))
+            dpf3=(np.cos(np.radians(L3[idxL3][1]-C3[idxC3][1])))*100
+            L3_DPF.addreading((L3[idxL3][0],dpf3))
             idxL3+=1
             idxC3+=1    
         l1_DPF.addbounds(*changed_ranges["L1"])
