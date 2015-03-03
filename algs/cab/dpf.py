74ba91a322bb182adf48c8375cd545b403c3c67f
Connor Brooks <connoc706@gmail.com>
2015-03-02 23:37:27 -0800

    more algs in algorithm_fields, and dpf alg replacement

---
+++ algs/cab/dpf.py
@@ -0,0 +1,36 @@
+import qdf
+import numpy as np
+class DPF (qdf.QDF2Distillate):
+    def initialize(self, section, name):
+        self.set_section(section)
+        self.set_name(name)
+        self.set_version(1)
+        self.register_output("DPF", "Percent")
+        self.register_input("voltage_phase")
+        self.register_input("current_phase")
+
+    def compute(self, changed_ranges, input_streams, params, report):
+        DPF = report.output("DPF")
+        print "compute invoked:"
+        print "changed_ranges: ", changed_ranges
+        print "params: ", params
+        idx_voltage=0
+        idx_current=0
+
+        voltage = input_streams["voltage_phase"]
+        current = input_streams["current_phase"]
+
+        while idx_voltage < len(voltage) and idx_current < len(current) :
+            if current[idx_current][0] < voltage[idx_voltage][0]:
+                idx_current += 1
+            if voltage[idx_voltage][0] < current[idx_current][0]:
+                idx_voltage += 1
+
+            # compute cosin value of the differnece between voltage angle and current angle and dpf
+            dpf1=(np.cos(np.radians(voltage[idx_voltage][1]-current[idx_current][1])))*100
+            DPF.addreading(voltage[idx_voltage][0],dpf1)
+            idx_voltage += 1
+            idx_current += 1
+
+        DPF.addbounds(*changed_ranges["voltage_phase"])
+        DPF.addbounds(*changed_ranges["current_phase"])
--- tools/config_gen/algorithm_fields.py
+++ tools/config_gen/algorithm_fields.py
@@ -9,5 +9,29 @@ settings = {
   'phase_difference' : { 'path'    : 'cab.phase_difference.Phase_Difference',
                          'deps'    : ['phase1', 'phase2'],
                          'params'  : ['section', 'name'],
-                         'outputs' : ['phase_difference'] }
+                         'outputs' : ['phase_difference'] },
+
+  'fundamental_power': { 'path'    : 'cab.fundamental_power.Rundamental_Power',
+                         'deps'    : ['voltage_phase', 'current_phase', 'dpf'],
+                         'params'  : ['section', 'name'],
+                         'outputs' : ['Fundamental_Power'] },
+
+  'reactive_power'   : { 'path'    : 'cab.reactive_power.Reactive_Power',
+                         'deps'    : ['voltage_phase', 'current_phase'],
+                         'params'  : ['section', 'name'],
+                         'outputs' : ['Reactive_Power'] },
+
+   'dpf'             : { 'path'    : 'cab.dpf.DPF',
+                         'deps'    : ['voltage_phase', 'current_phase'],
+                         'params'  : ['section', 'name'],
+                         'outputs' : ['DPF'] },
+
+   'sequence'        : { 'path'    : 'alu.sequence.Sequence',
+                         'deps'    : ['M1', 'M2', 'M3', 'A1', 'A2', 'A3'],
+                         'params'  : ['section', 'name'],
+                         'outputs' : ['ZERO_SEQ_ANG', 'ZERO_SEQ_MAG',
+                                      'POSITIVE_SEQ_ANG', 'POSITIVE_SEQ_MAG',
+                                      'NEGATIVE_SEQ_ANG', 'NEGATIVE_SEQ_ANG',
+                                      'UNBALANCE_NEG_SEQ', 'UNBALANCE_ZERO_SEQ'] },
+
 }
