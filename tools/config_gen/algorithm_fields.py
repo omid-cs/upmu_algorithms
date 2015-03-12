303262fc46785691826e6d06b15289985169c2e3
Connor Brooks <connoc706@gmail.com>
2015-03-12 13:04:15 -0700

    added cleaning algorithm to algorithm set

--- tools/config_gen/algorithm_fields.py
+++ tools/config_gen/algorithm_fields.py
@@ -31,4 +31,12 @@ algorithms = {
                                       'POSITIVE_SEQ_ANG', 'POSITIVE_SEQ_MAG',
                                       'NEGATIVE_SEQ_ANG', 'NEGATIVE_SEQ_ANG',
                                       'UNBALANCE_NEG_SEQ', 'UNBALANCE_ZERO_SEQ'] },
+   'clean'           : { 'path'    : 'cab.clean.Clean',
+                         'deps'    : ["C1ANG", "C1MAG", "C2ANG", "C2MAG", "C3ANG", "C3MAG",
+                                      "L1ANG", "L1MAG", "L2ANG", "L2MAG", "L3ANG", "L3MAG",
+                                      "LSTATE"],
+                         'params'  : ['section', 'name'],
+                         'outputs' : ["C1ANG", "C1MAG", "C2ANG", "C2MAG", "C3ANG", "C3MAG",
+                                      "L1ANG", "L1MAG", "L2ANG", "L2MAG", "L3ANG", "L3MAG",
+                                      "LSTATE"] }
 }
