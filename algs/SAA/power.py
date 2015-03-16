6e7e8fd9ca7f0de618d3e64106826276d1a8a660
Shayaan Abdullah <sabdullah@berkeley.edu>
2015-03-15 17:56:32 -0700

    Fix typo and add fundamental power file

--- algs/SAA/power.py
+++ algs/SAA/power.py
@@ -5,7 +5,7 @@ class Power (qdf.QDF2Distillate):
   def initialize(self, section="Reactive_Power", name="reactive_power"):
     self.set_section(section)
     self.set_name(name)
-    self.set_version(5)
+    self.set_version(6)
     self.register_input("voltage_phase")
     self.register_input("current_phase")
     self.register_input("voltage_mag")
@@ -20,7 +20,7 @@ class Power (qdf.QDF2Distillate):
     current_mag = input_streams["current_mag"]
 
     reactive_power_output = report.output("reactive_power_output")
-    fundamenta_power_output = report.output("fundamental_power_output")
+    fundamental_power_output = report.output("fundamental_power_output")
     i_vol_phase = 0
     i_cur_phase = 0
     i_vol_mag = 0
@@ -46,7 +46,7 @@ class Power (qdf.QDF2Distillate):
       rp = voltage_mag[i_vol_mag][1]*current_mag[i_cur_mag][1]*np.sin(np.radians(voltage_phase[i_vol_phase][1]- voltage_phase[i_cur_phase][1]))
       #rp = np.sin(np.radians(voltage_phase[i_vol][1]-current_phase[i_cur][1])) #mult by magV and magC
       reactive_power_output.addreading(time, rp)
-      fundamenta_power_output.addreading(time, fp)
+      fundamental_power_output.addreading(time, fp)
 
       #increment counters and loop
       i_vol_mag += 1
