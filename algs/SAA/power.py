cb13866c666451ff09b32e9a596eceece6554e55
Shayaan Abdullah <sabdullah@berkeley.edu>
2015-03-16 17:58:13 -0700

    Change Angle difference in formula to float

--- algs/SAA/power.py
+++ algs/SAA/power.py
@@ -42,8 +42,8 @@ class Power (qdf.QDF2Distillate):
 
       # Calculate reactive power
       time = voltage_phase[i_vol_phase][0]
-      fp = voltage_mag[i_vol_mag][1]*current_mag[i_cur_mag][1]*np.cos(np.radians(voltage_phase[i_vol_phase][1]- voltage_phase[i_cur_phase][1]))
-      rp = voltage_mag[i_vol_mag][1]*current_mag[i_cur_mag][1]*np.sin(np.radians(voltage_phase[i_vol_phase][1]- voltage_phase[i_cur_phase][1]))
+      fp = voltage_mag[i_vol_mag][1]*current_mag[i_cur_mag][1]*np.cos(np.radians(float(voltage_phase[i_vol_phase][1])- float(voltage_phase[i_cur_phase][1])))
+      rp = voltage_mag[i_vol_mag][1]*current_mag[i_cur_mag][1]*np.sin(np.radians(float(voltage_phase[i_vol_phase][1])- float(voltage_phase[i_cur_phase][1])))
       #rp = np.sin(np.radians(voltage_phase[i_vol][1]-current_phase[i_cur][1])) #mult by magV and magC
       reactive_power_output.addreading(time, rp)
       fundamental_power_output.addreading(time, fp)
