8eb650fb9f4db00b2d6104b41db6849f2f2befb3
yijianlu <yijianlu@berkeley.edu>
2015-04-06 17:04:49 -0700

    Update Sequence_GP.py

--- prod/sequence/Sequence_GP.py
+++ prod/sequence/Sequence_GP.py
@@ -1,1 +1,76 @@
-1
+[ global ]
+algorithm = alu.sequence.sequence
+enabled = False
+
+[ Sequence_for_GP_Voltage ]
+  chunking = parallel
+  paramver = 1
+
+  mintime = 2014-12-01T00:00:00
+  maxtime = 2016-12-01T00:00:00
+
+  [[ deps ]]
+  #grizzly_new-L1
+  A1 = 3a0b4a12-5077-437f-8b3a-6f1370d867bf
+  #grizzly_new-L2
+  A2 = 7c56f343-5250-41e5-af5a-fd7115129a49
+  #grizzly_new-L3
+  A3 = d2aa0c10-ac64-4e8f-8bff-5c56787353f7
+  #grizzly_new-M1
+  M1 = 
+  #grizzly_new-M1
+  M2 = 
+  #grizzly_new-M3
+  M3 = 
+  
+  [[ params ]]
+  section = Production/LBNL/GP_BUS2/L1-E-ANG_C1-ANG
+  name = DPF
+
+  [[ outputs ]]
+  
+  ZERO_SEQ_ANG = 
+  ZERO_SEQ_MAG =
+  POSITIVE_SEQ_ANG =
+  POSITIVE_SEQ_MAG =
+  NEGATIVE_SEQ_ANG =
+  NEGATIVE_SEQ_MAG =
+  UNBALANCE_NEG_SEQ =
+  UNBALANCE_ZERO_SEQ =
+  
+[ Sequence_for_GP_Current ]
+  chunking = parallel
+  paramver = 1
+
+  mintime = 2014-12-01T00:00:00
+  maxtime = 2016-12-01T00:00:00
+
+  [[ deps ]]
+  
+  #grizzly_new-C1
+  A1 = a8959600-0554-41c7-acad-1cdf0b73beaa
+  #grizzly_new-C2
+  A2 = 78bff774-6325-41a4-b250-c12095d6b2e6
+  #grizzly_new-C3
+  A3 = 3500a90e-12ae-4451-8dad-f3497a16eef6
+  #grizzly_new-M1
+  M1 = 
+  #grizzly_new-M1
+  M2 = 
+  #grizzly_new-M3
+  M3 = 
+  
+  [[ params ]]
+  section = Production/LBNL/GP_BUS2/L2-E-ANG_C2-ANG
+  name = DPF
+
+  [[ outputs ]]
+  ZERO_SEQ_ANG = 
+  ZERO_SEQ_MAG =
+  POSITIVE_SEQ_ANG =
+  POSITIVE_SEQ_MAG =
+  NEGATIVE_SEQ_ANG =
+  NEGATIVE_SEQ_MAG =
+  UNBALANCE_NEG_SEQ =
+  UNBALANCE_ZERO_SEQ =
+
