552cb9f8a1cc2558e0603a76e4d37476a2aa774d
Shayaan Abdullah <sabdullah@berkeley.edu>
2015-04-08 12:09:27 -0700

    Add Switch A-6

--- algs/SAA/power.py
+++ algs/SAA/power.py
@@ -5,14 +5,14 @@ class Power (qdf.QDF2Distillate):
   def initialize(self, section="Reactive_Power", name="reactive_power"):
     self.set_section(section)
     self.set_name(name)
-    self.set_version(10)
+    self.set_version(11)
     self.register_input("voltage_phase")
     self.register_input("current_phase")
     self.register_input("voltage_mag")
     self.register_input("current_mag")
     self.register_output("reactive_power_output", "VAR")
     self.register_output("fundamental_power_output", "W")
-    self.register_output("phasediff_output","rad")
+    #self.register_output("phasediff_output","rad")
 
   def compute(self, changed_ranges, input_streams, params, report):
     voltage_phase = input_streams["voltage_phase"]
@@ -22,7 +22,7 @@ class Power (qdf.QDF2Distillate):
 
     reactive_power_output = report.output("reactive_power_output")
     fundamental_power_output = report.output("fundamental_power_output")
-    phasediff_output = report.output("phasediff_output")
+    #phasediff_output = report.output("phasediff_output")
     i_vol_phase = 0
     i_cur_phase = 0
     i_vol_mag = 0
@@ -46,10 +46,10 @@ class Power (qdf.QDF2Distillate):
       time = voltage_phase[i_vol_phase][0]
       fp = voltage_mag[i_vol_mag][1]*current_mag[i_cur_mag][1]*np.cos(np.radians(voltage_phase[i_vol_phase][1])- np.radians(current_phase[i_cur_phase][1]))
       rp = voltage_mag[i_vol_mag][1]*current_mag[i_cur_mag][1]*np.sin(np.radians(voltage_phase[i_vol_phase][1])- np.radians(current_phase[i_cur_phase][1]))
-      pd = np.radians(voltage_phase[i_vol_phase][1])- np.radians(current_phase[i_cur_phase][1])
+      #pd = np.radians(voltage_phase[i_vol_phase][1])- np.radians(current_phase[i_cur_phase][1])
       #rp = np.sin(np.radians(voltage_phase[i_vol][1]-current_phase[i_cur][1])) #mult by magV and magC
       #create new output for testing this is angle difference between voltage and current
-      phasediff_output.addreading(time,pd)
+      #phasediff_output.addreading(time,pd)
       reactive_power_output.addreading(time, rp)
       fundamental_power_output.addreading(time, fp)
 
@@ -67,4 +67,4 @@ class Power (qdf.QDF2Distillate):
     fundamental_power_output.addbounds(*changed_ranges["current_phase"])
     fundamental_power_output.addbounds(*changed_ranges["voltage_mag"])
     fundamental_power_output.addbounds(*changed_ranges["current_mag"])
-    phasediff_output.addbounds(*changed_ranges["voltage_phase"])
+    #phasediff_output.addbounds(*changed_ranges["voltage_phase"])
