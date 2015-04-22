0cec9906eec2b3070387202b99d2ef94a64d9b7f
Shayaan Abdullah <sabdullah@berkeley.edu>
2015-04-22 14:12:45 -0700

    Clean up correlation algorithm

--- algs/SAA/correlation.py
+++ algs/SAA/correlation.py
@@ -5,7 +5,7 @@ class Correlation(qdf.QDF2Distillate):
 	def initialize(self, section , name ):
 		self.set_section(section)
 		self.set_name(name)
-		self.set_version(4)
+		self.set_version(5)
 		self.register_input("Signal1")
 		self.register_input("Signal2")
 		self.register_output("correlation_output","none")
@@ -17,29 +17,33 @@ class Correlation(qdf.QDF2Distillate):
 		Signal1 = input_streams["Signal1"]
 		Signal2 = input_streams["Signal2"]
 		correlation_output = report.output("correlation_output")
-		i_correlation = 0
 		i_Signal1 = 0
 		i_Signal2 = 0
 		windowsize = 10
-		while i_Signal1 < (len(Signal1) - 2*windowsize) and i_Signal2 < (len(Signal2) - 2*windowsize): #this should be replaced with a time restriction
-			#construct a signal for which 10 elements are lined up so that the correlation may be calculated
-			#first construct array of n number of elements which line up
-			i_Signal1_array = []
-			i_Signal2_array = []
-			#600 elements
-			#line up window
-			while len(i_Signal1_array)< windowsize and len(i_Signal2_array) < windowsize:
-				#if the time at a particular index of signal 1 is not equal to the time at the index of signal 2 increment the index of one of the
-				# signal for which the time is lower
-				if not (Signal1[i_Signal1][0] == Signal2[i_Signal2][0]):
-					max_time = max(Signal1[i_Signal1][0],Signal2[i_Signal2][0])
-					if Signal1[i_Signal1][0] < max_time:
+
+		i_Signal1_array = []
+		i_Signal2_array = []
+
+		while len(i_Signal1_array)< windowsize and len(i_Signal2_array) < windowsize:
+		#if the time at a particular index of signal 1 is not equal to the time at the index of signal 2 increment the index of one of the
+		# signal for which the time is lower
+			if not (Signal1[i_Signal1][0] == Signal2[i_Signal2][0]):
+				max_time = max(Signal1[i_Signal1][0],Signal2[i_Signal2][0])
+				if Signal1[i_Signal1][0] < max_time:
 						i_Signal1 += 1
-					elif Signal2[i_Signal2][0] < max_time:
+				elif Signal2[i_Signal2][0] < max_time:
 						i_Signal2 += 1
-				else:
-					i_Signal1_array.extend([i_Signal1])
-					i_Signal2_array.extend([i_Signal2])
+			else:
+				i_Signal1_array.extend([i_Signal1])
+				i_Signal2_array.extend([i_Signal2])
+
+		i_Signal1end = i_Signal1_array[-1]
+		i_Signal2end = i_Signal2_array[-1]
+		i_Signal1 = i_Signal1_array[0]
+		i_Signal2 = i_Signal2_array[0]
+		
+		while i_Signal1end < (len(Signal1)) and i_Signal2end < (len(Signal2)): #this should be replaced with a time restriction
+
 
 			#once window has been lined up perform calculation
 			windowed_signal1 = [Signal1[i][1] for i in i_Signal1_array]
@@ -49,10 +53,31 @@ class Correlation(qdf.QDF2Distillate):
 			window_starttime = Signal1[i_Signal1_array[0]][0]
 			window_endtime = Signal1[i_Signal1_array[-1]][0]
 			correlation_output.addreading(window_starttime,co)
-			#shift window over by 1 step and recalculate covariance
-			i_Signal1 = i_Signal1_array[1]
-			i_Signal2 = i_Signal2_array[1]
-			i_correlation += 1
+			#shift over start of window
+			i_Signal1_array = [1:]
+			i_Signal2_array = [1:]
+			i_Signal1 = i_Signal1_array[-1]
+			i_Signal2 = i_Signal2_array[-1]
+			#find suitable end point of window by finding index at which they line up
+
+			while len(i_Signal1_array)< windowsize and len(i_Signal2_array) < windowsize:
+			#if the time at a particular index of signal 1 is not equal to the time at the index of signal 2 increment the index of one of the
+			# signal for which the time is lower
+				if i_Signal1 > len(Signal1) or i_Signal2 > len(Signal2):
+					break
+				if not (Signal1[i_Signal1][0] == Signal2[i_Signal2][0]):
+					max_time = max(Signal1[i_Signal1][0],Signal2[i_Signal2][0])
+					if Signal1[i_Signal1][0] < max_time:
+							i_Signal1 += 1
+					elif Signal2[i_Signal2][0] < max_time:
+							i_Signal2 += 1
+				else:
+					i_Signal1_array.extend([i_Signal1])
+					i_Signal2_array.extend([i_Signal2])
+
+			i_Signal1end = i_Signal1_array[-1]
+			i_Signal2end = i_Signal2_array[-1]
+
 		
 
 		outliers_output.addbounds(*changed_ranges["Signal1"])
